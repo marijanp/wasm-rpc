4befe1b138d2068ee4b857d815b490549ab0a650
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-08-23 15:34:00 +0200

    drop

--- wasm-rpc/build.rs
+++ wasm-rpc/build.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 use std::process::Stdio;
 
 fn main() -> Result<()> {
-    //let wasm_ast_root = find_package_root("golem-wasm-ast");
+    let wasm_ast_root = find_package_root("golem-wasm-ast");
 
     let mut config = prost_build::Config::new();
     config.extern_path(".wasm.ast", "::golem_wasm_ast::analysis::protobuf");
@@ -19,27 +19,24 @@ fn main() -> Result<()> {
             "proto/wasm/rpc/witvalue.proto",
             "proto/wasm/rpc/type_annotated_value.proto",
         ],
-        &[&format!("./proto"), &"proto".to_string()],
+        &[&format!("{wasm_ast_root}/proto"), &"proto".to_string()],
     )?;
     Ok(())
 }
 
 fn find_package_root(name: &str) -> String {
-    let output = Command::new("cargo")
-        .arg("metadata")
-        .arg("--manifest-path")
-        .arg("./Cargo.toml")
-        .arg("--offline")
+    let output = Command::new("echo")
+        .arg("$CARGO")
         .output()  // Run the command and capture the output
         .expect("Failed to execute command");
     let stdout = String::from_utf8_lossy(&output.stdout);
-    println!("{}", stdout);
+    eprintln!("{}", stdout);
     let metadata = MetadataCommand::new()
         .manifest_path("./Cargo.toml")
         .verbose(true)
-        .other_options(vec!["--offline".to_string(), "--frozen".to_string()])
+        .other_options(vec!["--offline".to_string()])
         .exec()
-        .expect("giggi");
+        .unwrap();
     let package = metadata.packages.iter().find(|p| p.name == name).unwrap();
     package.manifest_path.parent().unwrap().to_string()
 }
